1de6b637f4d7dff87b2b1f165d32d1bb7f199ba7
PHLINT <phlint83@gmail.com>
2023-08-27 22:29:23 +0300

    Hello

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String offset = "\t\t\t\t";
+        System.out.print(
+                offset + "В лесу родилась ёлочка,\n" +
+                        offset + "В лесу она росла.\n" +
+                        offset + "Зимой и летом стройная,\n" +
+                        offset + "Зелёная была.\n" +
+                        "\n" +
+                        offset + "Метель ей пела песенку:\n" +
+                        offset + "«Спи, ёлочка, бай-бай!»\n" +
+                        offset + "Мороз снежком укутывал:\n" +
+                        offset + "«Смотри, не замерзай!»\n" +
+                        "\n" +
+                        offset + "Трусишка зайка серенький\n" +
+                        offset + "Под ёлочкой скакал.\n" +
+                        offset + "Порою волк, сердитый волк,\n" +
+                        offset + "Рысцою пробегал.\n" +
+                        "\n" +
+                        offset + "Чу! Снег по лесу частому\n" +
+                        offset + "Под полозом скрипит\n" +
+                        offset + "Лошадка мохноногая\n" +
+                        offset + "Торопится, бежит.\n" +
+                        "\n" +
+                        offset + "Везёт лошадка дровенки\n" +
+                        offset + "А в дровнях мужичок\n" +
+                        offset + "Срубил он нашу ёлочку\n" +
+                        offset + "Под самый корешок.\n" +
+                        "\n" +
+                        offset + "Теперь она, нарядная,\n" +
+                        offset + "На праздник к нам пришла,\n" +
+                        offset + "И много, много радости\n" +
+                        offset + "Детишкам принесла.\n");
+    }
+}
